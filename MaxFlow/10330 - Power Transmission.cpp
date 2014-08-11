388eb04fe24af59a145c544fe601ba3334520b93
ddxofy <contest1063@gmail.com>
2014-08-12 04:07:10 +0600

    Maximum Flow

---
+++ MaxFlow/10330 - Power Transmission.cpp
@@ -0,0 +1,169 @@
+//Md. Khairullah Gaurab
+//CSE, SUST  20th
+//gaurab.cse.sust@gmail.com
+
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <cstring>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+
+
+#define sf scanf
+#define pf printf
+#define mem(a,b) memset(a,b,sizeof(a))
+#define pb push_back
+#define all(a) (a.begin(),a.end())
+#define REP(i,a,b) for(int i=a; i<=b; ++i)
+#define REPR(i,a,b) for(int i=b; i>=a; --i)
+#define mp(a,b) make_pair(a,b)
+#define fs first
+#define sc second
+#define unq(vec) stable_sort(vec.begin(),vec.end());\
+                 vec.resize(distance(vec.begin(),unique(vec.begin(),vec.end())));
+
+using namespace std;
+
+const int INF = 1000000000;
+
+typedef long long Long;
+typedef double dd;
+
+struct Edge {
+  int from, to, cap, flow, index;
+  Edge(int from, int to, int cap, int flow, int index) :
+    from(from), to(to), cap(cap), flow(flow), index(index) {}
+};
+
+struct PushRelabel {
+  int N;
+  vector<vector<Edge> > G;
+  vector<Long> excess;
+  vector<int> dist, active, count;
+  queue<int> Q;
+
+  PushRelabel(int N) : N(N), G(N), excess(N), dist(N), active(N), count(2*N) {}
+
+  void AddEdge(int from, int to, int cap) {
+    G[from].push_back(Edge(from, to, cap, 0, G[to].size()));
+    if (from == to) G[from].back().index++;
+    G[to].push_back(Edge(to, from, 0, 0, G[from].size() - 1));
+  }
+
+  void Enqueue(int v) {
+    if (!active[v] && excess[v] > 0) { active[v] = true; Q.push(v); }
+  }
+
+  void Push(Edge &e) {
+    int amt = int(min(excess[e.from], Long(e.cap - e.flow)));
+    if (dist[e.from] <= dist[e.to] | amt == 0) return;
+    e.flow += amt;
+    G[e.to][e.index].flow -= amt;
+    excess[e.to] += amt;
+    excess[e.from] -= amt;
+    Enqueue(e.to);
+  }
+
+  void Gap(int k) {
+    for (int v = 0; v < N; v++) {
+      if (dist[v] < k) continue;
+      count[dist[v]]--;
+      dist[v] = max(dist[v], N+1);
+      count[dist[v]]++;
+      Enqueue(v);
+    }
+  }
+
+  void Relabel(int v) {
+    count[dist[v]]--;
+    dist[v] = 2*N;
+    for (int i = 0; i < G[v].size(); i++)
+      if (G[v][i].cap - G[v][i].flow > 0)
+    dist[v] = min(dist[v], dist[G[v][i].to] + 1);
+    count[dist[v]]++;
+    Enqueue(v);
+  }
+
+  void Discharge(int v) {
+    for (int i = 0; excess[v] > 0 && i < G[v].size(); i++) Push(G[v][i]);
+    if (excess[v] > 0) {
+      if (count[dist[v]] == 1)
+    Gap(dist[v]);
+      else
+    Relabel(v);
+    }
+  }
+
+  Long GetMaxFlow(int s, int t) {
+    count[0] = N-1;
+    count[N] = 1;
+    dist[s] = N;
+    active[s] = active[t] = true;
+    for (int i = 0; i < G[s].size(); i++) {
+      excess[s] += G[s][i].cap;
+      Push(G[s][i]);
+    }
+
+    while (!Q.empty()) {
+      int v = Q.front();
+      Q.pop();
+      active[v] = false;
+      Discharge(v);
+    }
+
+    Long totflow = 0;
+    for (int i = 0; i < G[s].size(); i++) totflow += G[s][i].flow;
+    return totflow;
+  }
+};
+
+
+int main(int argc, const char **argv)
+{
+    int test, N, M, source, sink, cap, u, v, B, D;
+    while(sf("%d",&N)==1)
+    {
+        source = 0;
+        sink = (2*N) + 1;
+        PushRelabel  obj(sink+2);
+        REP(j,1,N)
+        {
+            sf("%d",&cap);
+            obj.AddEdge(2*j-1,2*j,cap);
+        }
+        sf("%d",&M);
+        REP(j,1,M)
+        {
+            sf("%d %d %d",&u, &v, &cap);
+            obj.AddEdge(2*u, 2*v-1, cap);
+        }
+        sf("%d %d",&B, &D);
+        REP(j,1,B)
+        {
+            sf("%d",&u);
+            obj.AddEdge(source,2*u-1,(INF));
+        }
+        REP(j,1,D)
+        {
+            sf("%d",&u);
+            obj.AddEdge(2*u,sink,(INF));
+        }
+        pf("%lld\n",obj.GetMaxFlow(source,sink));
+    }
+    return 0;
+}
+
